95970108ba8aa940b6a668ddf3c106d66fbd6913
Nael Lammens <nslammens@gmail.com>
2023-12-09 13:13:32 +0100

    fixed the coroutine error of the sceneSmanager prefab

--- CentEgalUn_Unity/Assets/Scripts/Utilities/ScenesManager.cs
+++ CentEgalUn_Unity/Assets/Scripts/Utilities/ScenesManager.cs
@@ -6,20 +6,10 @@ using UnityEngine.SceneManagement;
 // attention de ne pas appelé ce script SceneManager (sans S) car ca ecrase une fonction deja existante dans unity
 public class ScenesManager : MonoBehaviour
 {
-    //static class will be kepts between loads thanks to dontdestroyonload function
-    public static ScenesManager manager;
 
     //the animator is here to have smoothe transitions between scene changes
     public Animator transitions;
-    //singleton stat
    
-
-    private void Start()
-    {
-
-    }
-
-
     //play first game function and manually set them in the inscpector
     public void PlayFirstGame() 
     {
@@ -37,14 +27,11 @@ public class ScenesManager : MonoBehaviour
     public void PlayThirdGame() 
     {
         StartCoroutine(PlayTransitionAnimation());
-        SceneManager.LoadScene("ThirdGameScene");
-        
+        SceneManager.LoadScene("ThirdGameScene"); 
     }
     
     public void QuitGame() 
     {
-        //the first line is to save game data, the second is to actually quit BUT it is private for now
-        //DataPersistenceManager.instance.OnApplicationQuit();
         Application.Quit();
     }
 
@@ -61,30 +48,5 @@ public class ScenesManager : MonoBehaviour
         //for a few seconds
         yield return new WaitForSeconds(1);
     }
-
-//FROM THE TEAM PANOPTES SLIDES: binary formatter not found
-    // public void Save() 
-    // {
-    //     BinaryFormatter bf = new BinaryFormatter();
-    //     FileStream file = File.Create(Application.persistentDataPath + "/gameInfo.dat");
-    //     GameData data = new GameData();
-    //     data.health = health;
-    //     data.player_name = player_name;
-    //     bf.Serialize(file, data);
-    //     file.Close();
-    // }
-
-    // public void Load() 
-    // {
-    //     if (File.Exists(Application.persistentDataPath + "/gameData.dat")) 
-    //     {
-    //         BinaryFormatter bf = new BinaryFormatter();
-    //         FileStream file = File.Open(Application.persistentDataPath + "/gameData.dat", FileMode.Open);
-    //         GameData data = (GameData) bf.Deserialize(file);
-    //         file.Close();
-    //         health = data.health;
-    //         player_name = data.player_name;
-    //     }
-    // }
 
 }
